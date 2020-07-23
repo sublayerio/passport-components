a4e444785846ed13f578582ec2555a49ec23a14e
keyflight <olivier@andev.nl>
2020-07-23 10:33:50 +0200

    fix: 🐛 disable demo for now

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-
-import Example from '../../src'
-
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 export default class Demo extends Component {
   render() {
     return <div>
       <h1>passport-components Demo</h1>
-      <Example/>
     </div>
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
